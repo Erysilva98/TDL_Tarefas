d13e0f689e15391f01d69d5cb3768fc631ca914e
Erysilva98 <erimilsonsilva98@gmail.com>
2022-11-10 21:06:17 -0300

    Escopo completo

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Button('xMark'),sg.Text('TAREFA'),sg.Input(''),sg.Button('EDITAR'),sg.Button('REMOVER')],
+    [sg.Checkbox(''),sg.CalendarButton('Data',key='-DATA-'),sg.Text('TAREFA'),sg.Input(''),sg.Button('EDITAR'),sg.Button('REMOVER')],
     [sg.Button('NOVA TAREFA')]
 ]
 
-janela = sg.Window('TO DO LIST',layout)
+janela = sg.Window('TDL - GERÊNCIE SUAS TAREFAS',layout)
 
-envent,value = janela.read()
+
+while True:
+    envent,value = janela.read()
+
+    if envent in ('Exit',sg.WIN_CLOSED):
+        janela.close()
+        break
+
+    print(envent,value)
