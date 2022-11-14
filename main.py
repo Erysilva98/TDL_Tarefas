2b013f0c8655a8d4cb64ce386b2828dcc996348e
Erysilva98 <erimilsonsilva98@gmail.com>
2022-11-14 16:04:56 -0300

    Metodo Deleta

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.CalendarButton("Informe Data", size=(10,1)),sg.T("-- -- -- --",key="-DATA-")],
+    [sg.CalendarButton("Informe Data", size=(10,1)),sg.T("-- -- -- --",key="-DATE-")],
     [sg.T("Ordem para:",size=(10,1)),sg.I(key="-USER-",font=("None 15"),size=(20,1))],
     [sg.T("Nova Tarefa:",size=(10,1)),sg.I(key="-TASK-",font=("None 15"),size=(32,1))],
-    [sg.Table(values='',headings=["Indice","Data","Nome","Tarefa"],key="-TABLE-",size=(500,10),auto_size_columns=False,col_widths=[5,9,20,30],vertical_scroll_only=False,justification='1',font="None 15")],
+    [sg.Table(values='',headings=["Indice","Data","Nome","Tarefa"],key="-TABLE-",enable_events=True,size=(500,10),auto_size_columns=False,col_widths=[5,9,20,30],vertical_scroll_only=False,justification='1',font="None 15")],
     [sg.B("Add",size=(10,1),button_color="green"),sg.B("Del",key="-DEL-",size=(10,1),button_color="red"),sg.Exit("Sair",size=(10,1))],
 
 ]
@@ -20,11 +20,20 @@ while True:
         break
 
     elif evento == "Add":
-        data = janela["-DATA-"].get().split()[0]
+        data = janela["-DATE-"].get().split()[0]
         tarefa = [[contador,data,valor["-USER-"],valor["-TASK-"]]]
         lTarefas += tarefa 
         janela["-TABLE-"].update(lTarefas)
         janela["-TASK-"].update('')
+        janela["-USER-"].update('')
         contador += 1
+    
+    elif evento == "-DEL-":
+        if valor["-TABLE-"]:
+            indice = valor["-TABLE-"][0]
+            del lTarefas[indice]
+            janela["-TABLE-"].update(lTarefas)
 
+    print(lTarefas)
+    print("\n")
     print(evento,valor)
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.CalendarButton("Set Date", size=(10,1)),sg.T("-- -- -- --",key="-DATA-")],
+    [sg.CalendarButton("Set Date", size=(10,1)),sg.T("-- -- -- --",key="-DATE-")],
     [sg.T("Write Task:",size=(10,1)),sg.I(key="-TASK-",font=("None 15"),size=(32,1))],
-    [sg.Table(values='',headings=["Index","Date","Task"],key="-TABLE-",size=(500,10),auto_size_columns=False,col_widths=[5,9,30],vertical_scroll_only=False,justification='1',font="None 15")],
+    [sg.Table(values='',headings=["Index","Date","Task"],key="-TABLE-",enable_events=True,size=(500,10),auto_size_columns=False,col_widths=[5,9,30],vertical_scroll_only=False,justification='1',font="None 15")],
     [sg.B("Add",size=(10,1),button_color="green"),sg.B("Delete",key="-DEL-",size=(10,1),button_color="red"),sg.Exit(size=(10,1))],
 
 ]
@@ -19,7 +19,7 @@ while True:
         break
 
     elif event == "Add":
-        date = window["-DATA-"].get().split()[0]
+        date = window["-DATE-"].get().split()[0]
         task = [[counter,date,values["-TASK-"]]]
         tasks += task 
         window["-TABLE-"].update(tasks)
