61d1fbd838a047d8128cac7227864da75437898d
Erysilva98 <erimilsonsilva98@gmail.com>
2022-11-14 12:56:20 -0300

    Metodo Add OK

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.CalendarButton("Data", size=(10,1)),sg.T("-- -- -- --", key="-DIA-")],
-    [sg.T("Escreva: ",size=(10,1)),sg.I(key="-TAR-",font=("None 15"),size=(32,1))],
-    [sg.Table(values='', headings=["INDICE","DATA","TAREFA"],key="-TABELA-",size=(500,10),auto_size_columns=False,col_widths=[7,9,30],vertical_scroll_only=False,justification='left',font="None 15")],
-    [sg.B("ADICIONAR",size=(10,1),button_color="green"),sg.B("REMOVER",key="-DEL-",button_color="red"),sg.Exit("FECHAR",size=(10,1))],
+    [sg.CalendarButton("Informe Data", size=(10,1)),sg.T("-- -- -- --",key="-DATA-")],
+    [sg.T("Nova Tarefa:",size=(10,1)),sg.I(key="-TASK-",font=("None 15"),size=(32,1))],
+    [sg.Table(values='',headings=["Indice","Data","Tarefa"],key="-TABLE-",size=(500,10),auto_size_columns=False,col_widths=[5,9,30],vertical_scroll_only=False,justification='1',font="None 15")],
+    [sg.B("Add",size=(10,1),button_color="green"),sg.B("Del",key="-DEL-",size=(10,1),button_color="red"),sg.Exit("Sair",size=(10,1))],
+
 ]
 
-#,sg.B("REMOVER",key="-DEL-"),sg.EXIT()
-
-janela = sg.Window('TDL - GERENCIE SUAS TAREFAS',layout)
+janela = sg.Window("TDL GERENCIE SUAS TAREFAS",layout)
+lTarefas = []
 contador = 1
 
 while True:
     evento, valor = janela.read()
-
-    if evento in ("FECHAR",sg.WIN_CLOSED):
+    if evento in ("Sair",sg.WIN_CLOSED):
         janela.close()
         break
-    elif evento == "ADICIONAR":
-        data = janela["-DIA"].get().split()[0]
-        tarefa = [[contador,data,valor["-TAR-"]]]
-        tarefa += tarefa
-        janela["-TABELA-"].update(tarefa)
-        contador+= 1
+
+    elif evento == "Add":
+        data = janela["-DATA-"].get().split()[0]
+        tarefa = [[contador,data,valor["-TASK-"]]]
+        lTarefas += tarefa 
+        janela["-TABLE-"].update(lTarefas)
+        janela["-TASK-"].update('')
+        contador += 1
 
     print(evento,valor)
